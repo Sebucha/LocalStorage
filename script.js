1a58eab4bf86383bb31e0b28e89f75d2505a45c3
Lukasz Formela <lukasz.formela@hpe.com>
2022-08-24 13:14:53 +0200

    chore(formatting): applied formatting once more

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import db, {getAdults} from "./db.js";
+import db, { getAdults } from "./db.js";
 
 let dataTable = document.getElementById("dataTable");
 let thead = document.createElement("thead");
@@ -10,51 +10,51 @@ const getData = (key) => JSON.parse(window.localStorage.getItem(key))
 
 setData(db)
 // Example of hot to use exported functions
-console.log({adults: getAdults()})
+console.log({ adults: getAdults() })
 
-const createRow = ({id, name, uId, age, sex, isDeletable = false}) => {
-	let row = document.createElement("tr");
-	row.setAttribute("id", id);
-	row.className = "table-row";
+const createRow = ({ id, name, uId, age, sex, isDeletable = false }) => {
+  let row = document.createElement("tr");
+  row.setAttribute("id", id);
+  row.className = "table-row";
 
-	let idCell = document.createElement("td");
-	let nameCell = document.createElement("td");
-	let uidCell = document.createElement("td");
-	let ageCell = document.createElement("td");
-	let sexCell = document.createElement("td");
+  let idCell = document.createElement("td");
+  let nameCell = document.createElement("td");
+  let uidCell = document.createElement("td");
+  let ageCell = document.createElement("td");
+  let sexCell = document.createElement("td");
 
-	idCell.innerHTML = id;
-	nameCell.innerHTML = name;
-	uidCell.innerHTML = uId;
-	ageCell.innerHTML = age;
-	sexCell.innerHTML = sex;
+  idCell.innerHTML = id;
+  nameCell.innerHTML = name;
+  uidCell.innerHTML = uId;
+  ageCell.innerHTML = age;
+  sexCell.innerHTML = sex;
 
-	[idCell, nameCell, uidCell, ageCell, sexCell].forEach(element => {
-		row.appendChild(element);
-	});
+  [idCell, nameCell, uidCell, ageCell, sexCell].forEach(element => {
+    row.appendChild(element);
+  });
 
-	if (isDeletable) {
+  if (isDeletable) {
     // DeleteRow doesn't neet to exist in the upper scope
     function deleteRow() {
       let row = document.getElementById(id);
       tbody.removeChild(row);
     }
 
-		let deleteButton = document.createElement("button");
-		deleteButton.addEventListener("click", deleteRow);
-		deleteButton.innerHTML = "x";
-		row.appendChild(deleteButton);
-	}
+    let deleteButton = document.createElement("button");
+    deleteButton.addEventListener("click", deleteRow);
+    deleteButton.innerHTML = "x";
+    row.appendChild(deleteButton);
+  }
 
-	return row;
+  return row;
 };
 
 const createTableInnerElements = () => {
-  thead.append(createRow({id:"ID", name:"Name", uId:"User ID", age:"Age", sex:"Sex"}));
+  thead.append(createRow({ id: "ID", name: "Name", uId: "User ID", age: "Age", sex: "Sex" }));
 
   getData("APIdata").map(
-    element => createRow({...element, isDeletable:true})
-  ).forEach(row=>{
+    element => createRow({ ...element, isDeletable: true })
+  ).forEach(row => {
     tbody.appendChild(row)
   })
 
